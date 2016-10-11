17069e7134cffbb2c2111be6c7d8011b5576e483
Tiago Bai <tiagoxbai@gmail.com>
2016-10-11 11:25:33 -0300

    Renaming sass files to scss and adding theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+import 'hammerjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app works!';
